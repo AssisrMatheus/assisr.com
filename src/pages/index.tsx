c65c54d0b1f049984e852391d529c1d5cc88030b
Matheus Assis <assisr.matheus@gmail.com>
2020-03-18 10:10:02 -0300

    :sparkles: Listing posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,52 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import HomeLayout from '../components/homeLayout';
 
-export default () => {
-  return <HomeLayout>Hello world</HomeLayout>;
+export const query = graphql`
+  query SITE_INDEX_QUERY {
+    allMdx(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { published: { eq: true } } }
+    ) {
+      nodes {
+        id
+        excerpt(pruneLength: 250)
+        frontmatter {
+          title
+          date
+        }
+      }
+    }
+  }
+`;
+
+type IndexProps = {
+  data: {
+    allMdx: {
+      nodes: {
+        id: string;
+        excerpt: string;
+        frontmatter: {
+          title: string;
+          date: string;
+        };
+      }[];
+    };
+  };
 };
+
+const Index: React.FC<IndexProps> = ({ data }) => {
+  return (
+    <HomeLayout>
+      {data.allMdx.nodes.map(({ excerpt, frontmatter }) => (
+        <>
+          <h1>{frontmatter.title}</h1>
+          <p>{frontmatter.date}</p>
+          <p>{excerpt}</p>
+        </>
+      ))}
+    </HomeLayout>
+  );
+};
+
+export default Index;
